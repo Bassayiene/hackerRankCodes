10c3db384c5a3c71a53f74a99f01d6fe4154a9d3
= <=>
2025-10-15 20:45:42 +0100

    java string reverse

---
+++ Javastringreverse.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Javastringreverse {
+    public static boolean isPalindrome(String s) {
+        int l = s.length();
+
+        for (int i = 0 ; i < l/2 ; i++) {
+            if (s.charAt(i) != s.charAt(l-1-i)) {
+                return  false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+            
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+
+        if (isPalindrome(A)){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+                 
+    }
+}
+
--- SubStringComparison.java
+++ SubStringComparison.java
@@ -6,7 +6,7 @@ public static String getSmallestAndLargest(String s, int k) {
         String smallest = s.substring(0,k);
         String largest = s.substring(0,k);
 
-        for (int i=0; i< s.length() - k ; i++ ) {
+        for (int i=0; i< s.length() - k + 1 ; i++ ) {
             if (s.substring(i,i+k).compareTo(smallest) < 0) {
                 smallest = s.substring(i,i+k); 
             }
