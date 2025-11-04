4ca1bd6f2eaeac2773a0722fb75811fe04427dec
= <=>
2025-11-04 12:17:30 +0100

    some fixes

--- StringTokens.java
+++ StringTokens.java
@@ -1,21 +1,21 @@
-import java.io.*;
 import java.util.*;
 
 public class StringTokens {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        // Write your code here.
-        if (s.trim().isEmpty()) {
-            System.out.println(0);
-            return;
+        String s;
+        try (Scanner scan = new Scanner(System.in)) {
+            s = scan.nextLine();
+            // Write your code here.
+            if (s.trim().isEmpty()) {
+                System.out.println(0);
+                return;
+            }
         }
-        scan.close();
         String[] tokens = s.trim().split("[ !,?._'@;]+");
         System.out.println(tokens.length);
-        for (int i = 0; i<  tokens.length ; i++ ) {
-            System.out.println(tokens[i]);
+        for (String token : tokens) {
+            System.out.println(token);
         }
     }
 }
